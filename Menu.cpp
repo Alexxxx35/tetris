60f6e1a1f9f59b5da943a41702f199e58385f325
Alexxx35 <alexboury@hotmail.fr>
2021-03-23 17:17:46 +0100

    menu

--- Menu.cpp
+++ Menu.cpp
@@ -3,36 +3,49 @@
 #include "iostream"
 Menu::Menu(sf::RenderWindow &window)
 {
-    selectedItemIndex = 0;
-    _width = window.getSize().x;
-    _height = window.getSize().y;
-    font.loadFromFile("arial.ttf");
-    if (!font.loadFromFile("arial.ttf"))
+    if (!_font.loadFromFile("arial.ttf"))
     {
         throw 1;
     }
-
+    // if (!_texture.loadFromFile("src/web-button.png"))
+    // {
+    //     throw 1;
+    // }
+    _width = window.getSize().x;
+    _height = window.getSize().y;
+    _font.loadFromFile("arial.ttf");
+    _texture.loadFromFile("src/web-button.png", sf::IntRect(10, 10, 32, 32));
+    // _sprite.setTexture(_texture);
+    // _sprite.setColor(sf::Color::White);
+    // _sprite.setTextureRect(sf::IntRect(0, 0, 160, 30));
+    // _sprite.setPosition(100, 100);
     for (int i = 0; i < numberOfItems; i++)
     {
         if (i == 0)
         {
-            _text[i].setString("Start");
+            _text[i].setString("TETRIS");
         }
         if (i == 1)
         {
-            _text[i].setString("Option");
+            _text[i].setString("Start");
         }
         if (i == 2)
         {
+            _text[i].setString("Option");
+        }
+        if (i == 3)
+        {
             _text[i].setString("Exit");
         }
-        _text[i].setFont(font);
+        _text[i].setFont(_font);
         _text[i].setFillColor(sf::Color::White);
         _text[i].setPosition(sf::Vector2f(_width / 2, (i * _height / numberOfItems)));
         std::cout << (i * _height / numberOfItems) << std::endl;
     }
 }
-
+Menu::~Menu()
+{
+}
 int Menu::getSelectedItemIndex()
 {
     return selectedItemIndex;
@@ -45,6 +58,7 @@ void Menu::draw(sf::RenderWindow &window)
     {
 
         window.draw(_text[i]);
+        window.draw(_sprite);
     }
 }
 
@@ -55,11 +69,11 @@ sf::Text *Menu::getText()
 
 void Menu::MoveUp()
 {
-    if (selectedItemIndex < 0)
+    if (selectedItemIndex < 1)
     {
-        selectedItemIndex = 0;
+        selectedItemIndex = 1;
     }
-    else if (selectedItemIndex > 0)
+    else if (selectedItemIndex > 1)
     {
         _text[selectedItemIndex].setFillColor(sf::Color::White);
         selectedItemIndex--;
@@ -69,7 +83,7 @@ void Menu::MoveUp()
 
 void Menu::MoveDown()
 {
-    if (selectedItemIndex >= numberOfItems)
+    if (selectedItemIndex > numberOfItems)
     {
         selectedItemIndex = numberOfItems - 1;
     }
@@ -80,6 +94,13 @@ void Menu::MoveDown()
         _text[selectedItemIndex].setFillColor(sf::Color::Red);
     }
 }
-Menu::~Menu()
-{
-}
+
+// sf::Texture Menu::getTexture()
+// {
+//     return _texture;
+// }
+
+// sf::Sprite Menu::getSprite()
+// {
+//     return _sprite;
+// }
--- Menu.hh
+++ Menu.hh
@@ -6,15 +6,19 @@ class Menu
 {
 private:
     int selectedItem;
-    sf::Font font;
-    sf::Text _text[3];
+    sf::Font _font;
+    sf::Text _text[4];
     unsigned int _width;  //normally take value of window
     unsigned int _height; //normally take value of window
-    int const numberOfItems = 3;
-    int selectedItemIndex;
+    int const numberOfItems = 4;
+    int selectedItemIndex = 0;
+    sf::Texture _texture;
+    sf::Sprite _sprite;
 
 public:
     int getSelectedItemIndex();
+    sf::Texture getTexture();
+    sf::Sprite getSprite();
     void draw(sf::RenderWindow &window);
     sf::Text *getText();
     void MoveUp();
